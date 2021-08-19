a485eb3f4106d969e0bcf7ddf7aa23afec2312b5
mahoney0101 <mahoneyjames67@gmail.com>
2021-08-19 20:00:11 +0100

    F# Async

---
+++ Async.fsx
@@ -0,0 +1,25 @@
+let work =
+    async{
+        do! Async.Sleep 2000
+        do printfn "First"
+    }
+
+work
+|> Async.Start
+
+printfn "Second"
+
+work
+|> Async.RunSynchronously
+
+printfn "Second"
+
+let task =
+async{
+    let name = "james"
+    return name
+}
+|> Async.StartAsTask
+
+task
+
